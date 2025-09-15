3a3f3d351909cf213aef7ed464536d10834973a1
visitorbookhk <visitorbookhk@gmail.com>
2025-09-16 00:59:39 +0800

    Add files via upload

--- js/view.js
+++ js/view.js
@@ -58,6 +58,7 @@ function showConfirmModal(title, body, footer) {
 
 function showScanModal() {
   scanModal.hide();
+  if (html5QrcodeScanner.getState() == Html5QrcodeScannerState.PAUSED) html5QrcodeScanner.resume();
   scanModal.show();
 }
 
