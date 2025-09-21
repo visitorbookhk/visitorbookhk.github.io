808fe9604f49f7bfaee85ccc934032af22a461a3
visitorbookhk <visitorbookhk@gmail.com>
2025-09-22 01:16:26 +0800

    Add files via upload

--- js/const.js
+++ js/const.js
@@ -1,4 +1,4 @@
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbwJRdEXuBp4Fwi5524q3jPKvBpnFEE19knpees64Xf3IeYN082_9Q3_GitutlU9ZOeG/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbxef4iiXbzjak1ROm9CUCFJMydfj5RntbyIT0Jh2IR1Y4IWk6y3taRk1tYnShEmXfvH_g/exec';
 const YOUR_CLIENT_ID = '876557140858-2mc16osg1aojod8jqvgsa35mvck0a55p.apps.googleusercontent.com';
 const YOUR_REDIRECT_URI = 'https://visitorbookhk.github.io/register';
 const lang_op = {
