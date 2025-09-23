b8926344242fe28584ac1ec114d7a4568bd043bd
visitorbookhk <visitorbookhk@gmail.com>
2025-09-23 20:22:39 +0800

    Add files via upload

--- js/onload.js
+++ js/onload.js
@@ -27,7 +27,6 @@ function gasIsReg() {
       if (data.status=='0') {
         if (data.res.isValid == '2') {
           vname = data.res.name;
-          console.log(JSON.stringify(data,0,2));
           createQrView(data.res.code);
         } else if (data.res.isValid == '1') {
           createRegView(data.res);
